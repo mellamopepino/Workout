44f8f127ddcf85554dc22466c6431ec0f0512003
mellamopepino <delfinagarciav@gmail.com>
2023-03-18 21:59:58 -0300

    [INIT] Workout

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen font-bold text-9xl">
+      <h1>Workout</h1>
+    </div>
+  );
+}
